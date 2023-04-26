e9a3a7e3be269719cf033b4ae4966183d06744d9
vietnamnanocare <131868521+vietnamnanocare@users.noreply.github.com>
2023-04-26 18:11:17 +0700

    Update board-buttons.js

--- board-buttons.js
+++ board-buttons.js
@@ -3,18 +3,26 @@ TrelloPowerUp.initialize({
     return t.card('id', 'idList').get('idList')
       .then(function(listId) {
         // Update the custom buttons on the card based on the new list
-        if (listId === 'list-id-1') {
+        if (listId === '643a899f144aad0bd5ebb949') {
           // Show button 1 and hide button 2
           t.showCardButton('button-1');
           t.hideCardButton('button-2');
-        } else if (listId === 'list-id-2') {
+          t.hideCardButton('button-3');
+        } else if (listId === '643a8915c79c5a4d8d977f6f') {
           // Show button 2 and hide button 1
           t.showCardButton('button-2');
           t.hideCardButton('button-1');
+          t.hideCardButton('button-3');
+        } else if (listId === '643a8915c79c5a4d8d977f71') {
+         // Show button 2 and hide button 1
+          t.showCardButton('button-3');
+          t.hideCardButton('button-1');
+          t.hideCardButton('button-2');
         } else {
           // Hide both buttons
           t.hideCardButton('button-1');
           t.hideCardButton('button-2');
+          t.hideCardButton('button-3');
         }
       });
   }
